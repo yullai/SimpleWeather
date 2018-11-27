0c695c29342a27108e7da7a5a5e045aaee30e3f9
yullai <45323807+yullai@users.noreply.github.com>
2018-11-27 09:43:32 +0100

    First commit

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+
+      
+
+        public JsonResult GetWeather()
+        {
+            Weather weath = new Weather();
+            string city = "Gothenburg";
+            return Json(weath.getWeatherForcast(city), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast(string city)
+        {
+            string appid = "430586656e3071a2dbe0c0883ff5dbdb";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=" + appid + "&units=metric"; 
+           
+            //synchronous client.
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            
+            return jsonContent;
+        }
+
+    }
+}
